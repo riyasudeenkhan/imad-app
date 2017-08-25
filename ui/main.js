1218b9b30f39c39507efb6a29c6ce1a6a6331ed0
riyasudeenkhan <30774988+riyasudeenkhan@users.noreply.github.com>
2017-08-25 17:53:22 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,3 +5,8 @@ alert("main.js");
 var element=document.getElementById('content');
 
 element.innerHTML = "newValue";
+
+var img = document.getElementById('img');
+img.onClick = function() {
+    img.style.marginLeft='100px';
+}
