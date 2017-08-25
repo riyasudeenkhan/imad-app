100c8404889c4b998cf5c359c7e217c4bf458d4c
riyasudeenkhan <30774988+riyasudeenkhan@users.noreply.github.com>
2017-08-25 18:00:34 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -7,6 +7,13 @@ var element=document.getElementById('content');
 element.innerHTML = "newValue";
 
 var img = document.getElementById('img');
+
+var marginLeft = 0;
+function moveRight(){
+    marginLeft = marginLeft+1;
+    img.style.marginLeft=marginLeft+'px';
+}
+
 img.onClick = function() {
-    img.style.marginLeft='100px';
+    var interval = setInterval(moveRight, 50);
 }
