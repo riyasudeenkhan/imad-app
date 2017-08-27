dc797e45aab99db49bf39c3ac70cfa374df027a4
riyasudeenkhan <30774988+riyasudeenkhan@users.noreply.github.com>
2017-08-27 18:15:21 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,6 +3,15 @@ console.log('Loaded!');
 var button = document.getElementById("counter");
 var counter=0;
 
+xhttp = new XMLHttpRequest();
+xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+        // Typical action to be performed when the document is ready:
+        var count=document.getElementById("count");
+        count.innerHTML=xhttp.responseText;
+    }
+};
+
 button.onclick = function(){
     counter=counter+1;
     var count=document.getElementById("count");
